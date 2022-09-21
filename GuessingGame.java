87add35c9da4c64dfe5229385bc1112db47afd2b
Vivian Wei <vwei@smith.edu>
2022-09-20 22:51:28 -0400

    updated GuessingGame

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,21 +1,42 @@
+// import java.util.Scanner;
+
+// class GuessingGame {
+//     static int secretNumber = 4;
+//     public static void main(String[] args){
+//         System.out.println("Works!");
+//         Scanner input = new Scanner(System.in);
+//         int guess = input.nextInt();
+
+//         if (guess == secretNumber) {
+//             System.out.println(x: "Yay! You guessed right");
+//         }
+//         else{
+//             System.out.println("Sorry,incorrect guess.");
+//         }
+            
+        
+//         input.close();
+//     }
+        
+// }
 import java.util.Scanner;
 
 class GuessingGame {
-    static int secretNumber = 4;
-    public static void main(String[] args){
-        System.out.println("Works!");
-        Scanner input = new Scanner(System.in);
-        int guess = input.nextInt();
+    public static void main(String[] args) {
 
-        if (guess == secretNumber) {
-            System.out.println(x: "Yay! You guessed right");
-        }
-        else{
-            System.out.println("Sorry,incorrect guess.");
-        }
-            
+        int secretNumber = 4;
+
+        System.out.print("Please enter an integer between 0 and 10: ");
+        Scanner input = new Scanner(System.in);
+        int userGuess = input.nextInt();
         
+        if (userGuess == secretNumber) {
+            System.out.println("Yay! You guessed it");
+        } else {
+            System.out.println("Sorry, you didn't guess right");
+        }
+
         input.close();
+
     }
-        
 }
